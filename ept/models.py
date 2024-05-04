1773216d7b22ea6e992c77e3d666f45d11af2e37
Hongjunyeong <105625862+gaenyangi@users.noreply.github.com>
2024-05-04 13:01:19 +0900

    Update models.py
    changed the name of column into creating_state to clarify

--- ept/models.py
+++ ept/models.py
@@ -45,4 +45,4 @@ class User(db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    uploading_state = db.Column(db.Boolean, default=False)
+    creating_state = db.Column(db.Boolean, default=False)
